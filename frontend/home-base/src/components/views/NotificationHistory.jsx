286a4de2547f04991a86f80021b733f97667f765
Brittney Epperson <beppers87@hotmail.com>
2024-05-22 15:13:46 -0500

    Refactored NotificationHistory to use axiosInstance

--- frontend/home-base/src/components/views/NotificationHistory.jsx
+++ frontend/home-base/src/components/views/NotificationHistory.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import axiosInstance from '../Axios';
 import { Table } from 'react-bootstrap';
 import '../styles/NotificationHistoryStyles.css';
 
@@ -8,16 +8,19 @@ const NotificationHistory = () => {
     const [emailNotifications, setEmailNotifications] = useState([]);
 
     useEffect(() => {
-        const token = localStorage.getItem('user');
-        axios.get("http://localhost:8080/api/email-notifications/history",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-            .then(response => setEmailNotifications(response.data))
-            .catch(error => console.error('Error fetching email notification data:', error));
-    }, []);
+		fetchEmailNotifications();
+	}, []);
+
+	const fetchEmailNotifications = async () => {
+		try {
+			const response = await axiosInstance.get(
+				'/email-notifications/history'
+			);
+			setEmailNotifications(response.data);
+		} catch (error) {
+			console.error('Error fetching email notification data:', error);
+		}
+	};
 
     return (
         <div className='table-pane'>
