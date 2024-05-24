6ed5d4c17903aa8fff426eb2a03806e1ef25809d
Austin <kenjigw@gmail.com>
2024-05-24 16:30:09 -0500

    updates for emailservice

--- backend/home-base/src/main/java/org/launchcode/homebase/service/EmailService.java
+++ backend/home-base/src/main/java/org/launchcode/homebase/service/EmailService.java
@@ -1,6 +1,5 @@
 package org.launchcode.homebase.service;
 
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.sendgrid.helpers.mail.objects.Personalization;
 import org.json.JSONArray;
@@ -16,7 +15,6 @@ import org.springframework.stereotype.Service;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Email;
 import org.springframework.scheduling.annotation.Scheduled;
-
 
 import java.io.IOException;
 import com.sendgrid.*;
@@ -44,60 +42,19 @@ public class EmailService {
     private SerpApiService serpApiService;
 
     private SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-//    Bash commands for updating
-//    echo "sendgrid.env" >> .gitignore
-//    echo "export SENDGRID_API_KEY='YOUR_API_KEY'" > sendgrid.env
-//    source ./sendgrid.env
-
-
-//    public void sendEmail(int id, int filterId, String email, String filterChangeReminder, String s, Map<String, Object> templateData) throws Exception {
-//        try {
-//            Mail mail = new Mail();
-//
-//            Email from = new Email("kenjigw@gmail.com");
-//            mail.setFrom(from);
-//
-//            Email to = new Email("abwashingstl@gmail.com");
-//
-//            String subject = "test";
-//            mail.setSubject(subject);
-//
-//            Personalization personalization = new Personalization();
-//            personalization.addTo(to);
-//            personalization.setSubject(subject);
-//            personalization.addDynamicTemplateData("user", "Billy");
-//            personalization.addDynamicTemplateData("equipment", "Closet");
-//            personalization.addDynamicTemplateData("location", "Summer Home");
-//            personalization.addDynamicTemplateData("filter", "15x30 Allergy");
-//            mail.addPersonalization(personalization);
-//            mail.setTemplateId("d-1ca98b984e2e48ac9ae12fd3fb8b7aa1");
-//
-//            // Build the request and send the email
-//            Request request = new Request();
-//            request.setMethod(Method.POST);
-//            request.setEndpoint("mail/send");
-//            request.setBody(mail.build());
-//
-//            Response response = sg.api(request);
-//
-//            System.out.println(response.getStatusCode());
-//            System.out.println(response.getBody());
-//            System.out.println(response.getHeaders());
-//        } catch (IOException ex) {
-//            throw new Exception("Failed to send email: " + ex.getMessage());
-//        }
-//    }
+    //    Bash commands for updating
+    //    echo "sendgrid.env" >> .gitignore
+    //    echo "export SENDGRID_API_KEY='YOUR_API_KEY'" > sendgrid.env
+    //    source ./sendgrid.env
 
     public void sendEmail(EmailRequest emailRequest) throws Exception {
         try {
             Mail mail = new Mail();
-
             Email from = new Email("kenjigw@gmail.com");
             mail.setFrom(from);
+            Email to = new Email(emailRequest.getTo());
 
-            Email to = new Email(emailRequest.getTo()); // Use the 'to' field from the EmailRequest object
-
-            String subject = emailRequest.getSubject(); // Use the 'subject' field from the EmailRequest object
+            String subject = emailRequest.getSubject();
             mail.setSubject(subject);
 
             Personalization personalization = new Personalization();
@@ -107,18 +64,25 @@ public class EmailService {
             // Get the JSONObject template data
             JSONObject templateData = emailRequest.getTemplateData();
 
-            // Create a new JSONObject to hold the dynamic template data
-            JSONObject dynamicTemplateData = new JSONObject();
+            // Add substitutions for dynamic values
+            personalization.addSubstitution("{{user}}", templateData.getString("user"));
+            personalization.addSubstitution("{{equipment}}", templateData.getString("equipment"));
+            personalization.addSubstitution("{{location}}", templateData.getString("location"));
+            personalization.addSubstitution("{{size}}", templateData.getString("size"));
 
-            // Add dynamic template data from the JSONObject
-            Iterator<String> keys = templateData.keys();
-            while (keys.hasNext()) {
-                String key = keys.next();
-                personalization.addDynamicTemplateData(key, templateData.get(key));
+            // Construct the topResults string
+            StringBuilder topResultsBuilder = new StringBuilder();
+            JSONArray topResultsArray = templateData.getJSONArray("topResults");
+            for (int i = 0; i < topResultsArray.length(); i++) {
+                JSONObject result = topResultsArray.getJSONObject(i);
+                String title = result.getString("title");
+                String link = result.getString("link");
+                topResultsBuilder.append("<a href=\"").append(link).append("\">").append(title).append("</a><br>");
             }
-            System.out.println("templateData: " + templateData);
+            personalization.addSubstitution("{{topResults}}", topResultsBuilder.toString());
+
             mail.addPersonalization(personalization);
-            mail.setTemplateId("d-1ca98b984e2e48ac9ae12fd3fb8b7aa1"); // Ensure this is the correct template ID
+            mail.setTemplateId("d2a18082-5804-41a3-bc2e-d60ade2632c2");
 
             // Build the request and send the email
             Request request = new Request();
@@ -135,8 +99,6 @@ public class EmailService {
             throw new Exception("Failed to send email: " + ex.getMessage());
         }
     }
-
-
     private void logEmailNotification(int equipmentId, int filterId, String to, String subject, String message) {
 
         Equipment equipment = equipmentRepository.findById(equipmentId)
@@ -155,7 +117,6 @@ public class EmailService {
         emailNotificationRepository.save(emailNotification);
 
     }
-
     @Scheduled(cron = "0 0 5 * * ?") // Run every day at 5 am
     public void sendEmailsForDueFilters() {
         try {
@@ -199,7 +160,7 @@ public class EmailService {
         JSONArray topResultsArray = new JSONArray();
         if (serpApiResults != null && serpApiResults.has("shopping_results")) {
             JsonNode shoppingResults = serpApiResults.get("shopping_results");
-            System.out.println("Shopping Results: " + shoppingResults);
+            //System.out.println("Shopping Results: " + shoppingResults);
             int count = 1;
             for (JsonNode result : shoppingResults) {
                 if (count > 3) {
@@ -213,8 +174,7 @@ public class EmailService {
             }
         }
         jsonData.put("topResults", topResultsArray);
-        System.out.println("Template Data: " + jsonData.toString());
-
+        System.out.println("Template Data: " + jsonData);
 
         // Construct the EmailRequest object
         EmailRequest emailRequest = new EmailRequest(
@@ -239,69 +199,6 @@ public class EmailService {
         );
     }
 
-//    private void sendEmailForFilterChange(Filter filter, User user) throws Exception {
-//        // Create email content and subject
-//        System.out.println("Sending email for filter change...");
-//        System.out.println("User email: " + user.getEmail());
-//        System.out.println("Filter equipment name: " + filter.getEquipment().getName());
-//
-//        // Fetch top 3 Google Shopping results using SERP API
-//        String searchQuery = "filter size" + filter.getHeight() + "x" + filter.getWidth() + "x" + filter.getLength(); // Use equipment name as search query
-//        JsonNode serpApiResults = serpApiService.getGoogleShoppingResults(searchQuery);
-//
-//        // Prepare template data
-//        Map<String, Object> templateData = new HashMap<>();
-//        templateData.put("user", user.getUsername()); // Assuming User has a getUsername() method
-//        templateData.put("equipment", filter.getEquipment().getName());
-//        templateData.put("location", filter.getLocation()); // Assuming Filter has a getLocation() method
-//        templateData.put("size", filter.getHeight() + "x" + filter.getWidth() + "x" + filter.getLength()); // Assuming Filter has getHeight(), getWidth(), and getLength() methods
-//
-//        // Include top 3 results in the template data
-//        List<Map<String, String>> topResults = new ArrayList<>();
-//        if (serpApiResults != null && serpApiResults.has("shopping_results")) {
-//            JsonNode shoppingResults = serpApiResults.get("shopping_results");
-//            System.out.println("Shopping Results: " + shoppingResults);
-//            int count = 1;
-//            for (JsonNode result : shoppingResults) {
-//                if (count > 3) {
-//                    break; // Include only the top 3 results
-//                }
-//                Map<String, String> resultMap = new HashMap<>();
-//                resultMap.put("title", result.get("title").asText());
-//                resultMap.put("link", result.get("link").asText());
-//                topResults.add(resultMap);
-//                count++;
-//            }
-//        }
-//        templateData.put("topResults", topResults);
-//        System.out.println("TemplateData:" + templateData);
-//        // Construct the EmailRequest object
-//        EmailRequest emailRequest = new EmailRequest(
-//                filter.getEquipment().getId(),
-//                filter.getId(),
-//                user.getEmail(),
-//                "Filter Change Reminder",
-//                "Your filter for " + filter.getEquipment().getName() + " is due for change.",
-//                templateData
-//        );
-//
-//        // Send email
-//        sendEmail(emailRequest);
-//
-//        // Log email notification
-//        logEmailNotification(
-//                filter.getEquipment().getId(),
-//                filter.getId(),
-//                user.getEmail(),
-//                "Filter Change Reminder",
-//                "Your filter for " + filter.getEquipment().getName() + " is due for change."
-//        );
-//    }
-
-
 }
-
-
-
 
 
