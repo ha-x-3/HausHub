1da8baf67076a90e673be6a532b45f6b1f80de83
Brittney Epperson <beppers87@hotmail.com>
2024-07-09 11:05:09 -0500

    Added parameterStore Service and value holder

--- backend/home-base/src/main/java/org/launchcode/homebase/HomeBaseApplication.java
+++ backend/home-base/src/main/java/org/launchcode/homebase/HomeBaseApplication.java
@@ -1,6 +1,5 @@
 package org.launchcode.homebase;
 
-import org.launchcode.homebase.config.ParameterStoreConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,11 +10,4 @@ public class HomeBaseApplication {
 		SpringApplication.run(HomeBaseApplication.class, args);
 	}
 
-	String endpoint = ParameterStoreConfig.getParameterValue("/haus-wrangler/aws.rds.endpoint");
-	String dbUsername = ParameterStoreConfig.getParameterValue("/haus-wrangler/aws.rds.username");
-	String dbPassword = ParameterStoreConfig.getParameterValue("/haus-wrangler/aws.rds.password");
-	String serpApiKey = ParameterStoreConfig.getParameterValue("/haus-wrangler/serp.api.key");
-	String jwtSecret = ParameterStoreConfig.getParameterValue("/haus-wrangler/jwt.secret.key");
-	String jwtExpiration = ParameterStoreConfig.getParameterValue("/haus-wrangler/jwt.expiration");
-	String jwtRefreshTokenExpiration = ParameterStoreConfig.getParameterValue("/haus-wrangler/jwt.refresh.token.expiration");
 }
--- backend/home-base/src/main/java/org/launchcode/homebase/service/ParameterService.java
+++ backend/home-base/src/main/java/org/launchcode/homebase/service/ParameterService.java
@@ -1,2 +1,26 @@
-package org.launchcode.homebase.service;public class ParameterService {
+package org.launchcode.homebase.service;
+
+import org.launchcode.homebase.config.ParameterValueHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParameterService {
+    private final ParameterValueHolder parameterValueHolder;
+
+    @Autowired
+    public ParameterService(ParameterValueHolder parameterValueHolder) {
+        this.parameterValueHolder = parameterValueHolder;
+    }
+
+    public void retrieveParameters() {
+        String endpoint = parameterValueHolder.getEndpoint();
+        String dbUsername = parameterValueHolder.getDbUsername();
+        String dbPassword = parameterValueHolder.getDbPassword();
+        String serpApiKey = parameterValueHolder.getSerpApiKey();
+        String jwtSecret = parameterValueHolder.getJwtSecret();
+        String jwtExpiration = parameterValueHolder.getJwtExpiration();
+        String jwtRefreshTokenExpiration = parameterValueHolder.getJwtRefreshTokenExpiration();
+
+    }
 }
