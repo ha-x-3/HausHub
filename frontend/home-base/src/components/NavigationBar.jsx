98d4bf86ba38a2d0b66477a360017b4eb5ae76cf
Brittney Epperson <beppers87@hotmail.com>
2024-07-09 11:47:48 -0500

    Added base URL to NavBar links

--- frontend/home-base/src/components/NavigationBar.jsx
+++ frontend/home-base/src/components/NavigationBar.jsx
@@ -23,46 +23,81 @@ const NavigationBar = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-sm fixed-top">
-      <a className="nav-link" href="/">
-        <img src={HausWrangler} width="auto" height="50" className="d-inline-block" alt="HausWrangler Logo" />
-        <img src={HausWranglerText} width="auto" height="35" className="d-inline-block" alt="HausWrangler Text" style={{ paddingTop: '3px' }} />
-      </a>
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <a className="nav-link" href="/filter-change">
-            Replace Filter
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/filter-history">
-            Filter History
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/notification-history">
-            Notification History
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/edit">
-            Edit
-          </a>
-        </li>
-      </ul>
-      <div className="nav-user">
-        {user ? (
-          <>
-            <span className="nav-item nav-link">Welcome, {user}</span>
-            <button onClick={handleLogout}>Logout</button>
-          </>
-        ) : (
-          <li className="nav-item">
-            <Link className="button" to="/login">Login</Link>
-          </li>
-        )}
-      </div>
-    </nav>
+		<nav className='navbar navbar-expand-sm fixed-top'>
+			<a
+				className='nav-link'
+				href='/'
+			>
+				<img
+					src={HausWrangler}
+					width='auto'
+					height='50'
+					className='d-inline-block'
+					alt='HausWrangler Logo'
+				/>
+				<img
+					src={HausWranglerText}
+					width='auto'
+					height='35'
+					className='d-inline-block'
+					alt='HausWrangler Text'
+					style={{ paddingTop: '3px' }}
+				/>
+			</a>
+			<ul className='navbar-nav mr-auto'>
+				<li className='nav-item'>
+					<a
+						className='nav-link'
+						href='/HausWrangler/filter-change'
+					>
+						Replace Filter
+					</a>
+				</li>
+				<li className='nav-item'>
+					<a
+						className='nav-link'
+						href='/HausWrangler/filter-history'
+					>
+						Filter History
+					</a>
+				</li>
+				<li className='nav-item'>
+					<a
+						className='nav-link'
+						href='/HausWrangler/notification-history'
+					>
+						Notification History
+					</a>
+				</li>
+				<li className='nav-item'>
+					<a
+						className='nav-link'
+						href='/HausWrangler/edit'
+					>
+						Edit
+					</a>
+				</li>
+			</ul>
+			<div className='nav-user'>
+				{user ? (
+					<>
+						<span className='nav-item nav-link'>
+							Welcome, {user}
+						</span>
+						<button onClick={handleLogout}>Logout</button>
+					</>
+				) : (
+					<li className='nav-item'>
+						<Link
+							className='button'
+							to='/HausWrangler/login'
+						>
+							Login
+						</Link>
+					</li>
+				)}
+			</div>
+		</nav>
   );
 };
 
