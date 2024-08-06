a49d1a117a6e09ddcfd6a51685f111cb1db1f69e
Brittney Epperson <beppers87@hotmail.com>
2024-08-06 14:54:59 -0500

    Added @Value annotations for parameters

--- backend/home-base/src/main/java/org/launchcode/homebase/service/JwtService.java
+++ backend/home-base/src/main/java/org/launchcode/homebase/service/JwtService.java
@@ -3,6 +3,7 @@ package org.launchcode.homebase.service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
